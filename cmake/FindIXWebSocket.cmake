944e9fda3bc81ba93760def9efc3d71c40d5abdf
Patient Zero <protean.p0@gmail.com>
2019-07-29 16:43:08 -0400

    Set up build; added trivial feed client

---
+++ cmake/FindIXWebSocket.cmake
@@ -0,0 +1,41 @@
+# This module tries to find LIBIXWEBSOCKET library and include files
+#
+# LIBIXWEBSOCKET_INCLUDE_DIR, path where to find LIBIXWEBSOCKET.h
+# LIBIXWEBSOCKET_LIBRARY_DIR, path where to find LIBIXWEBSOCKET.so
+# LIBIXWEBSOCKET_LIBRARIES, the library to link against
+# LIBIXWEBSOCKET_FOUND, If false, do not try to use LIBIXWEBSOCKET
+#
+# This currently works probably only for Linux
+
+include(FindPackageHandleStandardArgs)
+SET ( LIBIXWEBSOCKET_FOUND FALSE )
+
+FIND_PATH ( LIBIXWEBSOCKET_INCLUDE_DIR NAMES ixwebsocket/IXWebSocket.h
+        HINTS /usr/local/include /usr/include
+        )
+
+FIND_LIBRARY ( LIBIXWEBSOCKET_LIBRARIES NAMES ixwebsocket libixwebsocket
+        HINTS /usr/local/lib /usr/lib
+        )
+
+GET_FILENAME_COMPONENT( LIBIXWEBSOCKET_LIBRARY_DIR ${LIBIXWEBSOCKET_LIBRARIES} PATH )
+
+IF ( LIBIXWEBSOCKET_INCLUDE_DIR )
+    IF ( LIBIXWEBSOCKET_LIBRARIES )
+        SET ( LIBIXWEBSOCKET_FOUND TRUE )
+    ENDIF ( LIBIXWEBSOCKET_LIBRARIES )
+ENDIF ( LIBIXWEBSOCKET_INCLUDE_DIR )
+
+
+IF ( LIBIXWEBSOCKET_FOUND )
+    MARK_AS_ADVANCED(
+            LIBIXWEBSOCKET_LIBRARY_DIR
+            LIBIXWEBSOCKET_INCLUDE_DIR
+            LIBIXWEBSOCKET_LIBRARIES
+    )
+ENDIF ( )
+
+find_package_handle_standard_args(LIBIXWEBSOCKET
+        DEFAULT_MSG
+        LIBIXWEBSOCKET_INCLUDE_DIR
+        LIBIXWEBSOCKET_LIBRARIES)
---
+++ src/coinbase_main.cpp
@@ -0,0 +1,49 @@
+// Copyright 2019 Kyle F. Downey
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <spdlog/spdlog.h>
+
+#include <cloudwall/crypto-mktdata/coinbase.h>
+#include <uvw/loop.hpp>
+
+using cloudwall::coinbase::marketdata::CoinbaseRawFeedClient;
+using cloudwall::core::marketdata::Channel;
+using cloudwall::core::marketdata::Currency;
+using cloudwall::core::marketdata::CurrencyPair;
+using cloudwall::core::marketdata::RawFeedMessage;
+
+int main(int argc, const char *argv[]) {
+    spdlog::info("Coinbase Pro feed handler <READY>");
+
+    auto ccy_pair = CurrencyPair(Currency("BTC"), Currency("USD"));
+    std::list<Channel> channels({
+            Channel("status", { }),
+            Channel("matches", ccy_pair),
+            Channel("ticker", ccy_pair)
+    });
+    auto sub = Subscription(channels);
+    const OnRawFeedMessageCallback& callback = [](const RawFeedMessage& msg) {
+      spdlog::info("Incoming message: {}", msg.get_raw_json());
+    };
+    auto client = CoinbaseRawFeedClient(sub, callback);
+    client.connect();
+
+    auto loop = uvw::Loop::getDefault();
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+    while (true) {
+        loop->run();
+    }
+#pragma clang diagnostic pop
+}
